f86967c4a88081a2a9626d0010cc2ee92706c8ef
Alfred Fadel <theghost947@gmail.com>
2025-06-17 11:27:53 -0300

    Adding a create user form

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Team from "./scenes/team";
 // import Invoices from "./scenes/global/invoices";
 import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/global/bar";
-// import Form from "./scenes/global/form";
+import Form from "./scenes/form";
 // import Line from "./scenes/global/line";
 // import Pie from "./scenes/global/pie";
 // import FAQ from "./scenes/global/faq";
@@ -32,14 +32,14 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/team" element={<Team />} />
             <Route path="/contacts" element={<Contacts />} />
-            {/* <Route path="/invoices" element={<Invoices />} />
+            {/* <Route path="/invoices" element={<Invoices />} /> */}
             <Route path="/form" element={<Form />} />
-            <Route path="/bar" element={<Bar />} />
-            <Route path="/pie" element={<Pie />} />
-            <Route path="/line" element={<Line />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/geography" element={<Geography />} />
-            <Route path="/calendar" element={<Calendar />} />   */}
+            {/* <Route path="/bar" element={<Bar />} /> */}
+            {/* <Route path="/pie" element={<Pie />} /> */}
+            {/* <Route path="/line" element={<Line />} /> */}
+            {/* <Route path="/faq" element={<FAQ />} /> */}
+            {/* <Route path="/geography" element={<Geography />} /> */}
+            {/* <Route path="/calendar" element={<Calendar />} /> */}
           </Routes>
         </main>
       </div>
